8feb3db9c93ce0566b4f255350c2f4779925e439
Umesh Panwar <93350522+panwar28@users.noreply.github.com>
2023-10-30 08:52:39 -0400

    demo tf config files

---
+++ var.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this demo"
+  default    = "demo"
+}
+
+variable "location" {
+  description = "The Azure Region where all resources should be created."
+  default     = "eastus"
+}
